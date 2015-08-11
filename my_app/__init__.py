a0f1e91cfa02beec0be98ba6135330c57929a3d4
Mellomar Otarra <otarram@gmail.com>
2015-08-12 00:02:51 +0800

    Update

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import configuration
 from my_app.product.views import product_blueprint
 
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config.from_object(configuration.DevelopmentConfig)
 app.register_blueprint(product_blueprint)
 

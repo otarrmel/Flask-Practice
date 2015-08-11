13a1c749e0d7cfffb2e7d210e1fc455ca9de6c62
Mello <otarra.mellomar@navagis.com>
2015-08-11 19:35:10 +0800

    Product

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 import configuration
+from my_app.product.views import product_blueprint
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 app.config.from_object(configuration.DevelopmentConfig)
+app.register_blueprint(product_blueprint)
 
-from my_app.app_one import views
-from my_app.app_one import models
--- my_app/app_one/views.py
+++
@@ -1,8 +0,0 @@
-from my_app import app
-from my_app.app_one.models import MESSAGE
-
-
-@app.route('/')
-@app.route('/index')
-def hello_world():
-    return MESSAGE['default']
